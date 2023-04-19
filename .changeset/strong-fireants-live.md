b174883daffa59bef5ed395c274ed3420e790103
Andrew Dicks <AndrewDicks@users.noreply.github.com>
2023-04-19 13:54:49 +0100

    Deprecate @figlet-ts/cli and republish as figlet-ts (#5)

---
+++ .changeset/strong-fireants-live.md
@@ -0,0 +1,6 @@
+---
+'@figlet-ts/cli': minor
+'figlet-ts': patch
+---
+
+Deprecate @figlet-ts/cli in favour of package name figlet-ts
---
+++ packages/cli/deprecated.mjs
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log('\nThis package is deprecated.  Please uninstall this package and install figlet-ts instead, e.g. \n\n\tnpm uninstall -g @figlet-ts/cli\n\tnpm install -g figlet-ts\n\n\tyarn global remove @figlet-ts/cli\n\tyarn global add figlet-ts\n');
